c97ad60310487bd81616c72d6552809ae3a098bc
MuhammadSyahAlFikri_056 <fikriibabangdaffa@gmail.com>
2024-07-23 15:06:02 +0700

    First Push

---
+++ TechnicalTest/Assets/Scripts/BounceCube.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceCube : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    [SerializeField] Vector3 direction;
+    [SerializeField] private Material[] cubeMaterials;
+    [SerializeField] private int materialIndex = 0;
+    [SerializeField] private Renderer cubeRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cubeRenderer = GetComponent<Renderer>();
+        direction = Random.onUnitSphere;
+        direction.z = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collide");
+        Vector3 normal = collision.GetContact(0).normal;
+        direction = Vector3.Reflect(direction, normal);
+        ChangeMaterial();
+    }
+
+    private void ChangeMaterial()
+    {
+
+        if (cubeMaterials.Length > 0)
+        {
+            if (materialIndex >= cubeMaterials.Length)
+            {
+                materialIndex = 0;
+            }
+
+            materialIndex = materialIndex + 1;
+            cubeRenderer.material = cubeMaterials[materialIndex];
+        }
+    }
+}
